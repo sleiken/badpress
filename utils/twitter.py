fe023ec0b11d1b8581ac6fcb7709afd9fcaef600
Sam Leiken <sam.k.leiken@gmail.com>
2016-11-15 23:54:15 -0800

    Clean up tweet code

--- utils/twitter.py
+++ utils/twitter.py
@@ -1,6 +1,4 @@
 import tweepy
-import csv
-import numpy as np
 from textblob import TextBlob
 
 consumer_key= 'F279JL3R75Vg1XPYhXmAXQeAI'
@@ -13,11 +11,12 @@ api = tweepy.API(auth)
 
 def analyze_tweets(term):
     public_tweets = api.search(term)
-    polarity = []
-    subjectivity = []
+    polarity = 0
+    subjectivity = 0
+    num_tweets = len(public_tweets)
     for tweet in public_tweets:
         analysis = TextBlob(tweet.text)
-        polarity.append(analysis.polarity)
-        subjectivity.append(analysis.subjectivity)
-    scores = [sum(polarity) / len(polarity), sum(subjectivity) / len(subjectivity)]
+        polarity += analysis.polarity
+        subjectivity += analysis.subjectivity
+    scores = [polarity / num_tweets, subjectivity / num_tweets]
     return ['%.2f' % x for x in scores]
